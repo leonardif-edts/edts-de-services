da233303b9fd20aecdd8c0d17b3092a23429d3f8
leonardif-edts <leonardi.fabianto@sg-edts.com>
2024-07-01 18:15:37 +0700

    feat(spark): enabling HDFS secret getter in sample postgres

--- spark/scripts/sample/basic.py
+++ spark/scripts/sample/basic.py
@@ -1,17 +1,20 @@
+# Create New Session
 from pyspark.sql import SparkSession
-
 spark = SparkSession.Builder()\
     .appName("Sample - Basic")\
     .getOrCreate()
 
+# Generate Dummy Data
 data = [
     ('Leonardi', 'Fabianto', '1998-02-23', 'M', 2000),
     ('Agustinus', 'Vincentius', '2004-06-10', 'M', 500),
     ('Jesslyn', 'Jesslyn', '2000-11-13', 'F', 2000),
 ]
 columns = ["firstname", "lastname", "birthdate", "gender", "salary"]
-
 df = spark.createDataFrame(data, columns)
+
+# Print Dummy Data
 df.show()
 
+# Stop Instance
 spark.stop()
--- spark/scripts/sample/postgres_read.py
+++ spark/scripts/sample/postgres_read.py
@@ -1,19 +1,27 @@
+# Create New Session
 from pyspark.sql import SparkSession
-
 spark = SparkSession.Builder()\
     .appName("Sample - Postgres Read")\
     .getOrCreate()
 
+# Get Hadoop Conf
+conf = spark.sparkContext._jsc.hadoopConfiguration()
+creds_raw = conf.getPassword("database-postgres-cred")
+db_pass = "".join([str(creds_raw.__getitem__(i)) for i in range(creds_raw.__len__())])
+
+# Read from PostgreSQL
 df = spark.read\
     .format("jdbc")\
     .option("url", "jdbc:postgresql://database-postgres-1:5432/postgres")\
     .option("driver", "org.postgresql.Driver")\
     .option("user", "postgres")\
-    .option("password", "kLbORCO9irO8BGH")\
+    .option("password", db_pass)\
     .option("dbtable", "products")\
     .load()
 
+# Print Schema and Data
 df.printSchema()
 df.show()
 
+# Stop 
 spark.stop()
